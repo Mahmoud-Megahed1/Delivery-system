ed2746e63a157bcf91c2f0dea0038185c2db7765
Mahmoud Megahed <your_email@example.com>
2026-04-16 16:27:57 +0200

    Add AppUsersBankAccount model

---
+++ app/Models/AppUsersBankAccount.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AppUsersBankAccount extends Model
+{
+    use HasFactory;
+
+    protected $table = 'app_users_bank_accounts';
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(AppUser::class, 'user_id');
+    }
+}
